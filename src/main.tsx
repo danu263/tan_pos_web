9badc277f0cdbbb61a5b67e4b0973097967c12d1
alexmontes1215 <alex.montes@ext.blaze.me>
2024-06-19 21:33:38 -0500

    husky working!

--- src/main.tsx
+++ src/main.tsx
@@ -12,8 +12,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      {/*
-       */}
     </BrowserRouter>
   </React.StrictMode>,
 );
